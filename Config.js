f4ede59bc9c20f5e2d909d4010bb9dd497119faf
Hill-Sudani <71578540+Hill-Sudani@users.noreply.github.com>
2021-04-26 18:57:14 +0530

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+require("@firebase/firestore")
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCZwpByIbWA5reh69ewiefJA1XLzp-wbho",
+    authDomain: "story-hub-ec79c.firebaseapp.com",
+    projectId: "story-hub-ec79c",
+    storageBucket: "story-hub-ec79c.appspot.com",
+    messagingSenderId: "197165101488",
+    appId: "1:197165101488:web:b35aaae132acf3501be675"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase.firestore();
